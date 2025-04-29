c2c738f6b9428d7d64b9e5d71d32f6b9898ecca3
Sofía <sofiavillarejo.dev@gmail.com>
2025-04-29 12:49:25 +0200

    ultimo commit q no habia subido

---
+++ NugetBougies/DTO/RegisterModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NugetBougies.DTO
+{
+    public class RegisterModel
+    {
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public string Email { get; set; }
+        public string Passwd { get; set; }
+    }
+}
---
+++ NugetBougies/DTO/UserModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NugetBougies.DTO
+{
+    public class UserModel
+    {
+        public int IdUsuario { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public string Email { get; set; }
+        public string Imagen { get; set; }
+
+    }
+}
